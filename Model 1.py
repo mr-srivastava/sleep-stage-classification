b7e599f40100aae723877f15ec701316ac4fc7eb
mr-srivastava <aadarsh.srivastava.007@gmail.com>
2019-11-23 11:26:58 +0530

    Update Model 1.py

--- Model 1.py
+++ Model 1.py
@@ -7,14 +7,15 @@ Created on Fri Nov 22 14:54:47 2019
 import numpy as np
 from keras.callbacks import EarlyStopping
 from keras.models import Sequential
-from keras.layers import Dense
+import keras.layers
 import pandas as pd
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix 
 from sklearn.metrics import accuracy_score 
 from sklearn.metrics import classification_report
 #read in data using pandas
-train_df = pd.read_csv(r'C:\Users\Aadarsh Srivastava\OneDrive\Desktop\file_new.csv')
+train_df = pd.read_csv(r'C:\Users\Aadarsh Srivastava\OneDrive\Desktop\Project GIthub\Data CSV\file_new.csv')
 #check data has been read in properly
 train_df.head()
 
@@ -39,13 +40,15 @@ model = Sequential()
 
 #get number of columns in training data
 n_cols = x_train.shape[1]
-
+classes = 6
 #add model layers
-model.add(Dense(100, activation='relu', input_shape=(n_cols,)))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(1))
+model.add(keras.layers.Dense(100, activation='relu', input_shape=(n_cols,)))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.LeakyReLU(alpha=0.1))
+model.add(keras.layers.Dense(50, activation='relu'))
+model.add(keras.layers.Dense(10, activation='relu'))
+model.add(keras.layers.Dense(1, activation='softmax'))
+
 
 #compile model using mse as a measure of model performance
 model.compile(optimizer='adam', loss='mean_squared_error')
@@ -55,7 +58,7 @@ early_stopping_monitor = EarlyStopping(patience=3)
 #train model
 model.fit(x_train, y_train, validation_split=0.2, epochs=30, callbacks=[early_stopping_monitor])
 test_y_predictions = model.predict(x_test)
-test_y_predictions=np.round(test_y_predictions)
+#test_y_predictions=np.round(test_y_predictions)
 actual = y_test
 predicted =  test_y_predictions
 results = confusion_matrix(actual, predicted) 
