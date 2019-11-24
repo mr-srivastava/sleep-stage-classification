d6f3e2f68cfc89ff26175e930b13ea2f2ce5d6a9
mr-srivastava <aadarsh.srivastava.007@gmail.com>
2019-11-24 19:15:20 +0530

    Update Model 1.py
    Latest Accuracy Score = 75.27%

--- Model 1.py
+++ Model 1.py
@@ -8,6 +8,8 @@ import numpy as np
 from keras.callbacks import EarlyStopping
 from keras.models import Sequential
 import keras.layers
+from keras import losses
+from keras.utils import to_categorical
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
@@ -27,11 +29,10 @@ x.head()
 
 #create a dataframe with only the target column
 y = train_df[['hyp']]
-
+y = to_categorical(y)
 #for e in range(1,len(train_y)):
  #   train_y[e] = float(train_y[e])
 #view dataframe
-y.head()
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 
@@ -40,18 +41,17 @@ model = Sequential()
 
 #get number of columns in training data
 n_cols = x_train.shape[1]
-classes = 6
+classes = 7
 #add model layers
 model.add(keras.layers.Dense(100, activation='relu', input_shape=(n_cols,)))
 model.add(keras.layers.Dense(100, activation='relu'))
-model.add(keras.layers.LeakyReLU(alpha=0.1))
 model.add(keras.layers.Dense(50, activation='relu'))
 model.add(keras.layers.Dense(10, activation='relu'))
-model.add(keras.layers.Dense(1, activation='softmax'))
+model.add(keras.layers.Dense(classes, activation='softmax'))
 
 
 #compile model using mse as a measure of model performance
-model.compile(optimizer='adam', loss='mean_squared_error')
+model.compile(optimizer='sgd', loss=losses.categorical_crossentropy)
 
 #set early stopping monitor so the model stops training when it won't improve anymore
 early_stopping_monitor = EarlyStopping(patience=3)
@@ -61,9 +61,6 @@ test_y_predictions = model.predict(x_test)
 #test_y_predictions=np.round(test_y_predictions)
 actual = y_test
 predicted =  test_y_predictions
-results = confusion_matrix(actual, predicted) 
-print('Confusion Matrix :')
-print(results) 
 print('Accuracy Score :',accuracy_score(actual, predicted)*100)
 print('Report : ')
 print(classification_report(actual, predicted)) 
