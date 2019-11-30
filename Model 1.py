41e1c3b0e84303d5c32bccd73f69b5fca04aa043
mr-srivastava <aadarsh.srivastava.007@gmail.com>
2019-11-30 20:15:13 +0530

    Update Model 1.py

--- Model 1.py
+++ Model 1.py
@@ -7,17 +7,14 @@ Created on Fri Nov 22 14:54:47 2019
 import numpy as np
 from keras.callbacks import EarlyStopping
 from keras.models import Sequential
-import keras.layers
+from keras.layers import Conv1D,Flatten,Dropout,Dense
 from keras import losses
 from keras.utils import to_categorical
 import pandas as pd
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix 
-from sklearn.metrics import accuracy_score 
-from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score,accuracy_score,precision_score,recall_score
 #read in data using pandas
-train_df = pd.read_csv(r'C:\Users\Aadarsh Srivastava\OneDrive\Desktop\Project GIthub\Data CSV\file_new.csv')
+train_df = pd.read_csv(r'C:\Users\Aadarsh Srivastava\OneDrive\Desktop\Project Github\Data CSV\file_new.csv')
 #check data has been read in properly
 train_df.head()
 
@@ -29,38 +26,41 @@ x.head()
 
 #create a dataframe with only the target column
 y = train_df[['hyp']]
-y = to_categorical(y)
+y = pd.DataFrame(to_categorical(y))
+y = y.drop(y.columns[0],axis=1)
+y = y.rename_axis('ID').values
 #for e in range(1,len(train_y)):
  #   train_y[e] = float(train_y[e])
 #view dataframe
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=40)
 
+#Reshape Data
+x_train_cnn = x_train.rename_axis('ID').values
+x_train_cnn = np.reshape(x_train_cnn,(x_train_cnn.shape[0],x_train_cnn.shape[1],1))
+x_test_cnn = x_test.rename_axis('ID').values
+x_test_cnn = np.reshape(x_test_cnn,(x_test_cnn.shape[0],x_test_cnn.shape[1],1))
 #create model
 model = Sequential()
 
 #get number of columns in training data
-n_cols = x_train.shape[1]
+n_cols = x_train_cnn.shape[1]
 classes = 7
 #add model layers
-model.add(keras.layers.Dense(100, activation='relu', input_shape=(n_cols,)))
-model.add(keras.layers.Dense(100, activation='relu'))
-model.add(keras.layers.Dense(50, activation='relu'))
-model.add(keras.layers.Dense(10, activation='relu'))
-model.add(keras.layers.Dense(classes, activation='softmax'))
+model.add(Conv1D(filters=128,kernel_size=2,activation='relu',input_shape=(3,1)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(classes, activation='softmax'))
 
 
 #compile model using mse as a measure of model performance
-model.compile(optimizer='sgd', loss=losses.categorical_crossentropy)
+model.compile(optimizer='rmsprop', loss=losses.categorical_crossentropy,metrics=['accuracy'])
 
 #set early stopping monitor so the model stops training when it won't improve anymore
 early_stopping_monitor = EarlyStopping(patience=3)
 #train model
-model.fit(x_train, y_train, validation_split=0.2, epochs=30, callbacks=[early_stopping_monitor])
-test_y_predictions = model.predict(x_test)
+model.fit(x_train_cnn, y_train, validation_split=0.2, epochs=30, callbacks=[early_stopping_monitor])
+test_y_predictions = model.predict(x_test_cnn)
 #test_y_predictions=np.round(test_y_predictions)
 actual = y_test
 predicted =  test_y_predictions
-print('Accuracy Score :',accuracy_score(actual, predicted)*100)
-print('Report : ')
-print(classification_report(actual, predicted)) 
